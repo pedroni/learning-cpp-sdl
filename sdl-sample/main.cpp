d12adf3afc6c186093868f5ce7a1c3684c057483
pedroni <pedronidegodois@hotmail.com>
2025-09-21 16:30:54 -0300

    Initial Commit

---
+++ sdl-sample/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sdl-sample
+//
+//  Created by Lucas Pedroni on 21/09/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
